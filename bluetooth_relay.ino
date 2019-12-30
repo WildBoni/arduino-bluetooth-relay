9bf0c0a710afdcbb96db5dd6fe0ff4cb289eda2a
Davide Bonifacio <web@davidebonifacio.it>
2019-12-30 19:35:24 +0100

    adding pir functionality

--- bluetooth_relay.ino
+++ bluetooth_relay.ino
@@ -1,11 +1,17 @@
 #include <SoftwareSerial.h>
 
+// bluetooth
 int rxPin = 2;
 int txPin = 3;
+
 int ledPin = 5;
+int alarmPin = 6;
 int relayPin = 7;
+int pirPin = 8;
+int buzzPin = 9;
 
 String message;
+
 
 SoftwareSerial bluetooth(rxPin, txPin);
 
@@ -16,12 +22,15 @@ void setup()
   Serial.println("Comunicazione inizializzata");
 
   pinMode(ledPin, OUTPUT);
+  pinMode (alarmPin, OUTPUT);
+  pinMode (pirPin, INPUT);
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, HIGH);
 }
 void loop()
 {
   if(bluetooth.available()){
+    message = "";
     message+=char(bluetooth.read());
     Serial.println(message);
     delay(100);
@@ -42,6 +51,26 @@ void loop()
         digitalWrite(relayPin, HIGH);
         bluetooth.write("L");
         Serial.println("Led OFF");
+        message = "";
+      } else if(message == "P"){
+        digitalWrite(alarmPin, HIGH);
+        bluetooth.write("P");
+        if(digitalRead(pirPin) == HIGH) {
+          digitalWrite(ledPin, HIGH);
+          digitalWrite(relayPin, LOW);
+          Serial.println("PIR detection");          
+        } else {
+          digitalWrite(ledPin, LOW);
+          digitalWrite(relayPin, HIGH);
+          Serial.println("PIR listening");
+        }
+        delay(100);
+      } else if(message == "Q"){
+        digitalWrite(ledPin, LOW);
+        digitalWrite(alarmPin, LOW);
+        digitalWrite(relayPin, HIGH);
+        bluetooth.write("Q");
+        Serial.println("PIR OFF");
         message = "";
       }
     }
